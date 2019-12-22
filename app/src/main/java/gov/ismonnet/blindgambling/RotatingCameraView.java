a41cd4e0cec9c40faf3171b030798402eca501b8
Frarlo <ferlolovesnewmail@gmail.com>
2019-12-22 15:26:47 +0100

    Camera rotates correctly

--- app/src/main/java/gov/ismonnet/blindgambling/RotatingCameraView.java
+++ app/src/main/java/gov/ismonnet/blindgambling/RotatingCameraView.java
@@ -3,17 +3,22 @@ package gov.ismonnet.blindgambling;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.SurfaceTexture;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.hardware.Camera;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
 import org.opencv.core.Size;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import static android.content.ContentValues.TAG;
@@ -22,12 +27,14 @@ import static android.content.ContentValues.TAG;
 @SuppressLint("ObsoleteSdkInt")
 public class RotatingCameraView extends JavaCameraView {
 
-    private static final int MAGIC_TEXTURE_ID = 11;
+    protected final Activity mActivity;
 
-    private final Activity mActivity;
+    protected int mCameraId;
+    protected int mLastDisplayRotation;
+    protected int mActualCameraRotation;
 
-    private int mCameraId;
-    private SurfaceTexture mSurfaceTexture;
+    protected Bitmap mCacheBitmap;
+    protected CvCameraViewListener2 mListener;
 
     public RotatingCameraView(Context context, int cameraId) {
         super(context, cameraId);
@@ -68,24 +75,35 @@ public class RotatingCameraView extends JavaCameraView {
         }
 
         if(mCameraId == CAMERA_ID_ANY)
-            Log.e(TAG, "Coudln't find the actual camera id");
+            Log.e(TAG, "Couldn't find the actual camera id");
 
         return true;
     }
 
     @Override
-    @SuppressWarnings("SuspiciousNameCombination")
-    protected Size calculateCameraFrameSize(List<?> supportedSizes, ListItemAccessor accessor, int surfaceWidth, int surfaceHeight) {
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        super.surfaceChanged(holder, format, w, h);
 
-        final int actualCameraRotation = getCameraDisplayOrientation();
-        if(actualCameraRotation == 90 || actualCameraRotation == 270) {
-            final Size size = super.calculateCameraFrameSize(supportedSizes,
+        // Calculate the scale correctly
+
+        final boolean rotated = mActualCameraRotation == 90 || mActualCameraRotation == 270;
+        if (rotated && mScale != 0)
+            mScale = Math.min((float) getWidth() / mFrameHeight, (float) getHeight() / mFrameWidth);
+    }
+
+    @Override
+    @SuppressWarnings("SuspiciousNameCombination")
+    protected Size calculateCameraFrameSize(List<?> supportedSizes,
+                                            ListItemAccessor accessor,
+                                            int surfaceWidth,
+                                            int surfaceHeight) {
+
+        mActualCameraRotation = getCameraDisplayOrientation();
+        if(mActualCameraRotation == 90 || mActualCameraRotation == 270)
+            return super.calculateCameraFrameSize(supportedSizes,
                     accessor,
                     surfaceHeight,
                     surfaceWidth);
-            return new Size(size.height, size.width);
-        }
-
         return super.calculateCameraFrameSize(supportedSizes,
                 accessor,
                 surfaceWidth,
@@ -96,19 +114,24 @@ public class RotatingCameraView extends JavaCameraView {
     protected void AllocateCache() {
         super.AllocateCache();
 
+        // Get access to the allocated cache
+
+        try {
+            final Class<CameraBridgeViewBase> clazz = CameraBridgeViewBase.class;
+            final Field field = clazz.getDeclaredField("mCacheBitmap");
+            field.setAccessible(true);
+            mCacheBitmap = (Bitmap) field.get(this);
+        } catch (Exception ex) {
+            Log.e(TAG, "Couldn't get underlying bitmap instance using reflections", ex);
+            mCacheBitmap = null;
+        }
+
         // This is called just before starting the camera preview,
         // so I can use it to set parameters
 
         final Camera.Parameters params = mCamera.getParameters();
         changeParams(params);
         mCamera.setParameters(params);
-    }
-
-
-
-    protected void changeParams(Camera.Parameters params) {
-        final int actualCameraRotation = getCameraDisplayOrientation();
-        mCamera.setDisplayOrientation(actualCameraRotation);
     }
 
     protected int getCameraDisplayOrientation() {
@@ -118,6 +141,7 @@ public class RotatingCameraView extends JavaCameraView {
         Camera.getCameraInfo(mCameraId, info);
 
         final int rotation = mActivity.getWindowManager().getDefaultDisplay().getRotation();
+        mLastDisplayRotation = rotation;
 
         int degrees = 0;
         switch (rotation) {
@@ -138,5 +162,103 @@ public class RotatingCameraView extends JavaCameraView {
         return result;
     }
 
+    @Override
+    protected void deliverAndDrawFrame(CvCameraViewFrame frame) {
 
+        Canvas canvas = getHolder().lockCanvas();
+        if (canvas == null)
+            return;
+
+        final Mat modified;
+        if (mListener != null) {
+            modified = mListener.onCameraFrame(frame);
+        } else {
+            modified = frame.rgba();
+        }
+
+        boolean bmpValid = true;
+        if (modified != null) {
+            try {
+                Utils.matToBitmap(modified, mCacheBitmap);
+            } catch(Exception e) {
+                Log.e(TAG, "Mat type: " + modified);
+                Log.e(TAG, "Bitmap type: " + mCacheBitmap.getWidth() + "*" + mCacheBitmap.getHeight());
+                Log.e(TAG, "Utils.matToBitmap() throws an exception: " + e.getMessage());
+                bmpValid = false;
+            }
+        }
+
+        canvas.drawColor(0, android.graphics.PorterDuff.Mode.CLEAR);
+        if (!bmpValid && mCacheBitmap == null)
+            return;
+
+        // When rotating the screen upside down, the surface is the same size
+        // so surfaceChanged does not get invoked and the camera orientation is still the old one
+        // This forcefully sets it
+        final int display = mActivity.getWindowManager().getDefaultDisplay().getRotation();
+        if(mLastDisplayRotation != display)
+            mActualCameraRotation = getCameraDisplayOrientation();
+
+        final float scale = mScale == 0 ? 1 : mScale;
+
+        final boolean rotate = mActualCameraRotation == 90 || mActualCameraRotation == 270;
+        final float width = !rotate ? getWidth() : getHeight();
+        final float height = !rotate ? getHeight() : getWidth();
+
+        final int xToDraw = (int)((width - scale * mCacheBitmap.getWidth()) / 2);
+        final int yToDraw = (int)((height - scale * mCacheBitmap.getHeight()) / 2);
+        final int wToDraw = (int)((width - scale * mCacheBitmap.getWidth()) / 2 + scale * mCacheBitmap.getWidth());
+        final int hToDraw = (int)((height - scale * mCacheBitmap.getHeight()) / 2 + scale * mCacheBitmap.getHeight());
+
+        canvas.drawColor(0, android.graphics.PorterDuff.Mode.CLEAR);
+
+        if(rotate) {
+            canvas.translate(canvas.getWidth(), 0);
+            canvas.rotate(mActualCameraRotation);
+        } else {
+            canvas.rotate(mActualCameraRotation,
+                    getWidth() / 2f,
+                    getHeight() / 2f);
+        }
+
+        canvas.drawBitmap(mCacheBitmap,
+                new Rect(0, 0, mCacheBitmap.getWidth(), mCacheBitmap.getHeight()),
+                new Rect(xToDraw, yToDraw, xToDraw + wToDraw, yToDraw + hToDraw),
+                null);
+
+        if (mFpsMeter != null) {
+            mFpsMeter.measure();
+            mFpsMeter.draw(canvas, 20, 30);
+        }
+
+        getHolder().unlockCanvasAndPost(canvas);
+    }
+
+    @Override
+    public void setCvCameraViewListener(CvCameraViewListener2 listener) {
+        super.setCvCameraViewListener(listener);
+        mListener = getSuperListener();
+    }
+
+    @Override
+    public void setCvCameraViewListener(CvCameraViewListener listener) {
+        super.setCvCameraViewListener(listener);
+        mListener = getSuperListener();
+    }
+
+    protected CvCameraViewListener2 getSuperListener() {
+        try {
+            final Class<CameraBridgeViewBase> clazz = CameraBridgeViewBase.class;
+            final Field field = clazz.getDeclaredField("mListener");
+            field.setAccessible(true);
+            return (CvCameraViewListener2) field.get(this);
+        } catch (Exception ex) {
+            Log.e(TAG, "Couldn't get underlying CvCameraViewListener2 instance using reflections", ex);
+            return null;
+        }
+    }
+
+    protected void changeParams(Camera.Parameters params) {
+        mCamera.setDisplayOrientation(mActualCameraRotation);
+    }
 }
